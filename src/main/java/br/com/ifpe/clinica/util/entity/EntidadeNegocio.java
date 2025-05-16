53c43a2edc5747a3c1147fb4f4c30ba9172b1759
Cleiton <cleitonxav25@gmail.com>
2025-05-16 11:36:21 -0300

    Criação das Entidades-Auditavel e Negocio.

---
+++ src/main/java/br/com/ifpe/clinica/util/entity/EntidadeNegocio.java
@@ -0,0 +1,14 @@
+package br.com.ifpe.clinica.util.entity;
+
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+public abstract class EntidadeNegocio implements Serializable {
+    private Long id;
+    private Boolean habilitado;
+}
